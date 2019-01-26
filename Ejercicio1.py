624ed967eae2eb47e2a1aca3eb94f8a5ad457067
Marlon Maravilla <46935734+mmaravilla28@users.noreply.github.com>
2019-01-26 17:22:52 -0600

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,67 @@
+In [274]: palabra = ['H','o','l','a']
+
+In [275]: palabra
+Out[275]: ['H', 'o', 'l', 'a']
+
+In [276]: texto = ['H','o','l','a']
+
+In [277]: texto
+Out[277]: ['H', 'o', 'l', 'a']
+
+In [278]: texto.reverse()
+
+In [279]: texto
+Out[279]: ['a', 'l', 'o', 'H']
+
+In [280]: if palabra == texto :
+     ...:     print('Son iguales')
+     ...: else:
+     ...:     print('Son diferentes')
+     ...:
+Son diferentes
+
+In [281]: texto.reverse()
+
+In [282]: texto
+Out[282]: ['H', 'o', 'l', 'a']
+
+In [283]: if palabra == texto :
+     ...:     print('Son iguales')
+     ...: else:
+     ...:     print('Son diferentes')
+     ...:
+Son iguales
+
+In [284]: palabra2 = ['s','a','l','a','s']
+
+In [285]: palabra2
+Out[285]: ['s', 'a', 'l', 'a', 's']
+
+In [286]: texto2 = ['s','a','l','a','s']
+
+In [287]: texto2
+Out[287]: ['s', 'a', 'l', 'a', 's']
+
+In [288]: texto2.reverse()
+
+In [289]: texto2
+Out[289]: ['s', 'a', 'l', 'a', 's']
+
+In [290]: if palabra2 == texto2 :
+     ...:     print('Son iguales')
+     ...: else:
+     ...:     print('Son diferentes')
+     ...:
+Son iguales
+
+In [291]: texto2.reverse()
+
+In [292]: texto2
+Out[292]: ['s', 'a', 'l', 'a', 's']
+
+In [293]: if palabra2 == texto2 :
+     ...:     print('Son iguales')
+     ...: else:
+     ...:     print('Son diferentes')
+     ...:
+Son iguales
